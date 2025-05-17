5183499cda4d14e634d89d22f8efc4925b6995b7
Matt Wright <mjuni.dev@gmail.com>
2025-05-17 10:09:08 -0500

    removing implementation of context.Context

--- internal/core/server/interfaces/interfaces.go
+++ internal/core/server/interfaces/interfaces.go
@@ -10,13 +10,7 @@ type Router interface {
 }
 
 type Context interface {
-	// Err() error
 	String(code int, s string) error
-
-	// Value(key any) any
-
-	// Deadline() (t time.Time, ok bool)
-	// Done() <-chan struct{}
 }
 
 type HandlerFunc func(ctx Context) error
